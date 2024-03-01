73a9f4902d042b220769d4a1b97bcac8389a9c9a
middlemayson <middle.mayson@gmail.com>
2024-03-01 18:15:44 +0500

    complete search in explore

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,12 +1,16 @@
 import { photoList } from "../../components/array/photoList";
 import Photo from '../../components/photo/Photo';
+import search_icon from '../../img/elements/magnifying_glass.png';
 import './explore.css';
 
 const Explore = () => {
     return (
         <section className="explore">
             <div className="search">
-
+                <div className="search__input">
+                    <img src={search_icon} alt="" />
+                    <input name="search-input" defaultValue='Search users and hashtags' />
+                </div>
             </div>
             <div className="explore__img-grid">
                 {photoList.map((photo) => {
