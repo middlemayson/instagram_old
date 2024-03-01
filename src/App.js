861c957e81f96f41ee79066f0041da85a260c39a
middlemayson <middle.mayson@gmail.com>
2024-03-01 17:49:05 +0500

    add route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Header from './components/headers/Header';
 import Navbar from './components/navbar/Navbar';
 import Explore from './pages/explore/Explore';
@@ -8,10 +10,17 @@ import './style/reset.css';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Feed />
-      {/* <Explore /> */}
-      <Navbar />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Feed />} />
+          <Route path='/explore' element={<Explore />} />
+          
+        </Routes>
+        
+        <Navbar />
+      </Router>
+      
     </div>
   );
 }
